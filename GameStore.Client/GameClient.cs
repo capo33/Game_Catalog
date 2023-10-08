55d7eb10343bd1285ac3bfc18fd0d0553d77a273
Mohamed <s2000588@edu.bc.fi>
2023-10-08 20:50:33 +0300

    create class game and fetch all the game data

---
+++ GameStore.Client/GameClient.cs
@@ -0,0 +1,36 @@
+using GameStore.Client.Models;
+
+namespace GameStore.Client;
+
+public static class GameClient
+{
+    private static readonly List<Game> games = new()
+  {
+    new Game()
+    {
+      Id = 1,
+      Name = "Doom",
+      Genre = "FPS",
+      Price = 59.99m,
+      ReleaseDate = new DateTime(2016, 5, 13)
+    },
+    new Game()
+    {
+      Id = 2,
+      Name = "Doom Eternal",
+      Genre = "FPS",
+      Price = 59.99m,
+      ReleaseDate = new DateTime(2020, 3, 20)
+    },
+    new Game()
+    {
+      Id = 3,
+      Name = "Doom 3",
+      Genre = "FPS",
+      Price = 9.99m,
+      ReleaseDate = new DateTime(2004, 8, 3)
+    },
+  };
+
+  public static Game[] GetGames() => games.ToArray();
+}
